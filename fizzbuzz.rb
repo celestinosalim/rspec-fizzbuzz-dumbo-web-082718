c1a31c6a0b15ffdb10e94b27f51bbf24576b09e3
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 03:48:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
+    puts "FizzBuzz" # Go fizz
   elsif int % 5 == 0
-    "Buzz"
-  elsif (int % 3 == 0) && (int % 5 == 0)
-    return "FizzBuzz"
+    puts "Buzz"
+  elsif  int % 3 == 0
+    puts "Fizz"
   end
 end
